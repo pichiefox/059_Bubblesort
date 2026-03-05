a98a9901e6426424c6b44b36dcfdd3bf2a47199d
pichiefox <citrazad@gmail.com>
2026-03-05 17:56:48 +0700

    Process

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -20,4 +20,14 @@ void input ()
             cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
         }
     }
+    cout << endl;
+    cout << "===================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "===================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+    cout << "Data ke-" << (i + 1) << ": ";
+    cin >> arr [i]
+    }
 }
